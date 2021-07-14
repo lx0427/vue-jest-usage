1174dff292be2ee0d7ed7b025a8b7260b55a6052
lx0427 <lx15172413095@163.com>
2021-07-14 16:53:59 +0800

    jest配置，支持__tests__目录文件测试

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
+    preset: '@vue/cli-plugin-unit-jest',
+    roots: ['<rootDir>/src', '<rootDir>/tests'],
+    transform: {
+        '^.+\\.vue$': 'vue-jest',
+        '^.+\\js$': 'babel-jest',
+    },
+    testMatch: ['**/tests/**/?(*.)+(test).[jt]s?(x)', '**/tests/**/*spec.[jt]s?(x)', '**/__tests__/**/*.spec.js'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
 };
